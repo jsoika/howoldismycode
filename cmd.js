08ea933913bc1ac5d5e8ab01bf98f8f1a47a1846
Jerome Soika <>
2018-07-15 22:03:01 +0200

    removed unnecessary nodegit dependencies removed unused files fixed implementation using spawn

--- cmd.js
+++ cmd.js
@@ -1,39 +1,89 @@
 const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 var now = Math.floor(Date.now() / 1000);
+var items = [];
+var done = false;
+var task = [];
+var finishCallback = undefined;
+// function blameFile(directory, file) {
+//   console.log(directory);
+//   exec(
+//     "pwd",
+//     {
+//       cwd: directory
+//     },
+//     function(error, stdout, stderr) {
+//       // work with result
+//       exec(
+//         `git blame -t --date=unix --line-porcelain ${file}`,
+//         (error, stdout, stderr) => {
+//           if (error) {
+//             console.error(`exec error: ${error}`);
+//             return;
+//           }
+//           lines = stdout.split("\n");
+//           const timestamps = lines.filter(line =>
+//             line.includes("committer-time")
+//           );
+//           const times = timestamps.map(x => {
+//             return parseInt(x.split(" ")[1]);
+//           });
+//           var avg = times.reduce((a, b) => a + b) / times.length;
+//           var days = getTimeDiffInDays(avg, now);
+//           console.log("days: " + days);
+//           //   console.log(`stdout: ${stdout}`);
+//           //   console.log(`stderr: ${stderr}`);
+//         }
+//       );
+//     }
+//   );
+// }
 
-function blameFile(directory, file) {
-  console.log(directory);
-  exec(
-    "pwd",
-    {
-      cwd: directory
-    },
-    function(error, stdout, stderr) {
-      // work with result
-      exec(
-        `git blame -t --date=unix --line-porcelain ${file}`,
-        (error, stdout, stderr) => {
-          if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-          }
-          lines = stdout.split("\n");
-          const timestamps = lines.filter(line =>
-            line.includes("committer-time")
-          );
-          const times = timestamps.map(x => {
-            return parseInt(x.split(" ")[1]);
-          });
-          var avg = times.reduce((a, b) => a + b) / times.length;
-          var days = getTimeDiffInDays(avg, now);
-          console.log("days: " + days);
-          //   console.log(`stdout: ${stdout}`);
-          //   console.log(`stderr: ${stderr}`);
-        }
-      );
-    }
+function blameFileSpawn(dir, file) {
+  task.push({});
+  var child = spawn(
+    "git",
+    ["blame", "-t", "--date=unix", "--line-porcelain", file],
+    { cwd: dir }
   );
+  var result = "";
+  child.stdout.on("data", function(data) {
+    result += data.toString();
+    // console.log("stdout: " + data);
+  });
+
+  child.stderr.on("data", function(data) {
+    console.log("stderr: " + data);
+  });
+
+  child.on("close", function(code) {
+    lines = result.split("\n");
+    const timestamps = lines.filter(line => line.includes("committer-time"));
+    const times = timestamps.map(x => {
+      return parseInt(x.split(" ")[1]);
+    });
+    if (times.length > 0) {
+      var avg = times.reduce((a, b) => a + b) / times.length;
+      //var days = getTimeDiffInDays(avg, now);
+      var timeDiff = Math.abs(now - avg);
+
+      var days = timeDiff / 60 / 60 / 24;
+      days = Math.round((days + 0.00001) * 100) / 100;
+      items.push(days);
+      console.log(file + " " + days + " days");
+      // console.log("ndays: ", Math.ceil((now - avg) / (1000 * 3600)));
+      // console.log("days: ", now - Math.floor(avg));
+    }
+    task.pop();
+    if (task.length === 0) {
+      finishCallback();
+    }
+  });
+}
+
+function setFinishCallback(callback) {
+  finishCallback = callback;
 }
 
 function getTimeDiffInDays(timestamp, now) {
@@ -45,5 +95,8 @@ function getTimeDiffInDays(timestamp, now) {
 }
 
 module.exports = {
-  blameFile: blameFile
+  blameFile: blameFileSpawn,
+  items: items,
+  done: done,
+  setFinishCallback: setFinishCallback
 };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cmd = require("./cmd");
 var pathToRepo = require("path").resolve("../skipta-flic");
 var fs = require("fs");
 const excludes = ["node_modules", ".git", ".DS_Store", "thirdparty"];
+
 var traverseFileSystem = function(currentPath) {
   //   console.log(currentPath);
   var files = fs.readdirSync(currentPath);
@@ -9,7 +10,6 @@ var traverseFileSystem = function(currentPath) {
     var currentFile = currentPath + "/" + files[i];
     var stats = fs.statSync(currentFile);
     if (stats.isFile() && !excludes.includes(files[i])) {
-      console.log(currentFile);
       cmd.blameFile(currentPath, files[i]);
     } else if (stats.isDirectory() && !excludes.includes(files[i])) {
       traverseFileSystem(currentFile);
@@ -17,4 +17,17 @@ var traverseFileSystem = function(currentPath) {
   }
 };
 
+cmd.setFinishCallback(finish);
 traverseFileSystem(pathToRepo);
+
+function finish() {
+  // sum up
+  if (cmd.items.length > 0) {
+    var avg = cmd.items.reduce((a, b) => a + b) / cmd.items.length;
+    avg = Math.round((avg + 0.00001) * 100) / 100;
+    console.log("--------------------------------");
+    console.log("Total files: ", cmd.items.length);
+    console.log("Avg days: ", avg);
+    console.log("--------------------------------");
+  }
+}
